e640dcbd2c80d1f1c844a7c77134bc0e8664b886
Marcos <marcos@viafisio.com>
2024-07-17 20:43:02 +0200

    Enable Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className + " dark"}>{children}</body>
+      <body className={inter.className + " dark"}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
